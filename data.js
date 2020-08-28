dc3b8aae15e1a9f6e9a18129a171e484ef7601bf
kirperov <kirill.petrov.centrale@gmail.com>
2020-08-28 22:46:00 +0200

    add select sentences

--- data.js
+++ data.js
@@ -1,24 +1,52 @@
 let phrasesBlockOne = ["Avec", "Quelle que soit", "Tant que durera"];
-let phrasesBlockTwo = ["l'humanité", "la vie", "la situation"];
+let phrasesBlockTwo = ["l'humanité", "la vie", "la situation actuelle"];
 let phrasesBlockTree = ["les choses vont", "la crise va", "les déplacement vont"];
 let phrasesBlockFour = ["progresser", "s’ameliorer", "changer"];
 
 
 var divSentences = document.getElementById("showSentences");
-var number = 3;
+var btnGenerateOne = document.getElementById("generateOne");
+var btnGenerateTwo = document.getElementById("generateTwo");
+var selectNumbers = document.getElementById("numbers");
+
+var numberRand = 3;
+
+function generateRandomNumber(num) {
+      var result = Math.floor((Math.random() * num));
+      return result;
+}
+var numberSentences = selectNumbers.options[selectNumbers.selectedIndex].value;
+selectNumbers.addEventListener("change", function() {
+      var selectValue = selectNumbers.options[selectNumbers.selectedIndex].value;
+      numberSentences = selectValue;
+});
+ 
 function getRandomSentences() {
-      function generateRandomNumber(num) {
-            var result = Math.floor((Math.random() * num));
-            return result;
-      }
-      for(var i = 0; i < 3; i++) {
-            var text = phrasesBlockOne[generateRandomNumber(number)] + " " + phrasesBlockTwo[generateRandomNumber(number)] + " " + phrasesBlockTree[generateRandomNumber(number)]  + " " + phrasesBlockFour[generateRandomNumber(number)];
+      for(var i = 0; i < numberSentences; i++) {
             var newQuote = document.createElement("p");
+            var text = phrasesBlockOne[generateRandomNumber(numberRand)] + " " + phrasesBlockTwo[generateRandomNumber(numberRand)] + " " + phrasesBlockTree[generateRandomNumber(numberRand)]  + " " + phrasesBlockFour[generateRandomNumber(numberRand)];
+            newQuote.setAttribute("id", "quote"+i);
             divSentences.appendChild(newQuote);
-            newQuote.setAttribute("id", "quote"+i);  
-            document.getElementById("quote"+i).innerText = text;
+            document.getElementById("quote"+i).replaceWith(newQuote);
+            newQuote.innerText = text;   
+            var defaultText  =  document.getElementById("quote"+i).textContent;
+            var quotsLength = divSentences.childNodes.length;
+            if(quotsLength > numberSentences) {
+                  var newNumber = quotsLength - numberSentences;
+                  divSentences.removeChild(divSentences.childNodes[newNumber]);
+            }
+            console.log(defaultText);
       }
 }
+
+btnGenerateOne.addEventListener("click", function() {
+      getRandomSentences();
+});
+
+
+btnGenerateOne.addEventListener("click", function() {
+      getRandomSentences();
+});
 
 
  

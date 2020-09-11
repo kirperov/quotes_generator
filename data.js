524e95473468e4f8bc3bbdb44ef548fd7d4f5987
kirperov <kirill.petrov.centrale@gmail.com>
2020-09-11 12:34:16 +0200

    optimisation genetation quotes

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
-var quotes = {
-      quotesOne : {
-            quotesBlockOne: ["Avec", "Quelle que soit", "Tant que durera"],
-            quotesBlockTwo: ["l'humanité", "la vie", "le temps"],
-            quotesBlockTree: ["les choses vont", "le hommes vont", "la philosophie va"],
-            quotesBlockFour: ["progresser", "se développer", "changer"]
-      },
+var quotes = [
+      quotesOne = [
+            quotesBlockOne = ["Avec", "Quelle que soit", "Tant que durera"],
+            quotesBlockTwo = ["l'humanité", "la vie", "le temps"],
+            quotesBlockTree = ["les choses vont", "le hommes vont", "la philosophie va"],
+            quotesBlockFour = ["progresser", "se développer", "changer"]
+      ],
 
-      quotesTwo : {
-            quotesBlockOne: ["Le travail", "La haine", "L'amour"],
-            quotesBlockTwo: ["demande", "oblige", "impose"],
-            quotesBlockTree: ["de comprendre", "de changer", "à maitriser"],
-            quotesBlockFour: ["la vie", "les habitudes", "les choses"]
-      }
-}
+      quotesTwo = [
+            quotesBlockOne = ["Le travail", "La haine", "L'amour"],
+            quotesBlockTwo = ["demande", "oblige", "impose"],
+            quotesBlockTree = ["de comprendre", "de changer", "à maitriser"],
+            quotesBlockFour = ["la vie", "les habitudes", "les choses"]
+      ]
+]
+
 var divAlertError = document.createElement("div");
 var containerQuotes = document.getElementById("quotes"),
-     //containerError = document.getElementById("error"),
     btnGenerateQuote = document.getElementById("generate"),
     selectTheme = document.getElementById("themes"),
     selectNumbers = document.getElementById("numbers"),
     numberTheme = parseInt(selectTheme.options[selectTheme.selectedIndex].value),
     numberQuotes = parseInt(selectNumbers.options[selectNumbers.selectedIndex].value),
-    qgeneratedQuote = [],
+    generatedQuote = [],
     numberRand = 3;
 
 
@@ -37,33 +37,24 @@ selectTheme.addEventListener("change", function() {
       numberTheme = parseInt(this.value);
 });
 
-function generateRandomQuotes(choice) {
+
+function generateRandomQuotes(themeChoice) {
       var quotesLength = containerQuotes.childNodes.length;
-      if(choice > 0 && choice < 3 && numberQuotes > 0 && numberQuotes < 6) {
+      if(themeChoice >= 0 && themeChoice < 3 && numberQuotes > 0 && numberQuotes < 6) {
             divAlertError.remove();
             for(var i = 0; i < numberQuotes; i++) {
                   var newQuote = document.createElement("p");
                   newQuote.setAttribute("id", "quote"+i);
                   containerQuotes.appendChild(newQuote);
                   document.getElementById("quote"+i).replaceWith(newQuote);
-                        if(choice === 1) {
-                        qgeneratedQuote.push([
-                              quotes.quotesOne.quotesBlockOne[generateRandomNumber(numberRand)]  + " " +
-                              quotes.quotesOne.quotesBlockTwo[generateRandomNumber(numberRand)]  + " " +
-                              quotes.quotesOne.quotesBlockTree[generateRandomNumber(numberRand)] + " " +
-                              quotes.quotesOne.quotesBlockFour[generateRandomNumber(numberRand)]
-                        ]);
-                        newQuote.innerText = qgeneratedQuote;  
-                        } else if(choice === 2){
-                              qgeneratedQuote.push([
-                                    quotes.quotesTwo.quotesBlockOne[generateRandomNumber(numberRand)]  + " " +
-                                    quotes.quotesTwo.quotesBlockTwo[generateRandomNumber(numberRand)]  + " " +
-                                    quotes.quotesTwo.quotesBlockTree[generateRandomNumber(numberRand)] + " " +
-                                    quotes.quotesTwo.quotesBlockFour[generateRandomNumber(numberRand)]
-                              ]);
-                               newQuote.innerText = qgeneratedQuote;  
-                        }
-                  qgeneratedQuote = [];
+                  
+                  for(var n = 0; n <quotes[themeChoice].length; n++) {
+                        generatedQuote.push([quotes[themeChoice][n][generateRandomNumber(numberRand)]]);
+                  }
+                  generatedQuote = generatedQuote.join(' ');
+                  newQuote.innerText = generatedQuote; 
+
+                  generatedQuote = [];
             } 
       } else {
             containerQuotes.innerHTML = "";
